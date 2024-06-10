2d10c3b4741eb9f45efd77809bfcf8c7ef43ac0f
sultandilaram <sultanndilaram@gmail.com>
2024-06-10 20:02:26 +0000

    comment add backfill item db query

--- program_transformers/src/bubblegum/db.rs
+++ program_transformers/src/bubblegum/db.rs
@@ -138,32 +138,32 @@ where
     // a single row into the `backfill_items` table.  This way if an incomplete path was inserted
     // into `cl_items` due to an error, a gap will be created for the tree and the backfiller will
     // fix it.
-    if i - 1 == depth as i64 {
-        // See if the tree already exists in the `backfill_items` table.
-        let rows = backfill_items::Entity::find()
-            .filter(backfill_items::Column::Tree.eq(tree_id))
-            .limit(1)
-            .all(txn)
-            .await?;
+    // if i - 1 == depth as i64 {
+    //     // See if the tree already exists in the `backfill_items` table.
+    //     let rows = backfill_items::Entity::find()
+    //         .filter(backfill_items::Column::Tree.eq(tree_id))
+    //         .limit(1)
+    //         .all(txn)
+    //         .await?;
 
-        // If the tree does not exist in `backfill_items` and the sequence number is greater than 1,
-        // then we know we will need to backfill the tree from sequence number 1 up to the current
-        // sequence number.  So in this case we set at flag to force checking the tree.
-        let force_chk = rows.is_empty() && change_log_event.seq > 1;
+    //     // If the tree does not exist in `backfill_items` and the sequence number is greater than 1,
+    //     // then we know we will need to backfill the tree from sequence number 1 up to the current
+    //     // sequence number.  So in this case we set at flag to force checking the tree.
+    //     let force_chk = rows.is_empty() && change_log_event.seq > 1;
 
-        info!("Adding to backfill_items table at level {}", i - 1);
-        let item = backfill_items::ActiveModel {
-            tree: ActiveValue::Set(tree_id.to_vec()),
-            seq: ActiveValue::Set(change_log_event.seq as i64),
-            slot: ActiveValue::Set(slot as i64),
-            force_chk: ActiveValue::Set(force_chk),
-            backfilled: ActiveValue::Set(false),
-            failed: ActiveValue::Set(false),
-            ..Default::default()
-        };
+    //     info!("Adding to backfill_items table at level {}", i - 1);
+    //     let item = backfill_items::ActiveModel {
+    //         tree: ActiveValue::Set(tree_id.to_vec()),
+    //         seq: ActiveValue::Set(change_log_event.seq as i64),
+    //         slot: ActiveValue::Set(slot as i64),
+    //         force_chk: ActiveValue::Set(force_chk),
+    //         backfilled: ActiveValue::Set(false),
+    //         failed: ActiveValue::Set(false),
+    //         ..Default::default()
+    //     };
 
-        backfill_items::Entity::insert(item).exec(txn).await?;
-    }
+    //     backfill_items::Entity::insert(item).exec(txn).await?;
+    // }
 
     Ok(())
 }
